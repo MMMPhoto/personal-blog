8eac138886f98b728a0b5153c610d9e9cd63835b
Max McDonough <max.mcdonough@gmail.com>
2022-12-01 16:25:23 -0500

    Working Get request for all posts in react

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Post from '../components/Post';
+import { getAllPosts } from "../utils/api";
 
 export default function Home() {
+
+    const [publicPosts, setPublicPosts] = useState([{}]);
+
+    //  Fetch all public posts
+    useEffect(() => {
+        const fetchAllPosts = async () => {
+            try {
+                const response = await getAllPosts();
+                const jsonData = await response.json();
+                console.log(jsonData);
+            } catch (error) {
+                console.log("error", error);
+            };
+        };
+        fetchAllPosts();
+    }, []);
 
     return (
         <div>
