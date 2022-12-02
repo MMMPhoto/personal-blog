8eeec114fd139e5bf60a38d2a876b20075d12d86
Max McDonough <max.mcdonough@gmail.com>
2022-12-02 12:01:12 -0500

    Working link from each post to page with single post

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -1,12 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Post({postData}) {
 
     return (
         <div className="m-4">
-            <h2>{postData.title}</h2>
-            <p>{postData.body}</p>
-            <img src="https://picsum.photos/200/300" alt="random placeholder" />
+            <Link to={`/posts/${postData.id}`} style={{ textDecoration: 'none' }}>
+                <h2>{postData.title}</h2>
+                <p>{postData.body}</p>
+                <img src="https://picsum.photos/200/300" alt="random placeholder" />
+            </Link>
         </div>
     );
 };
