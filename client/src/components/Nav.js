2f86c2bebb7ff3507d778b7ea88fb20408b95640
Max McDonough <max.mcdonough@gmail.com>
2022-12-02 12:15:43 -0500

    Adjusted link styling

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,16 +1,17 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import './Nav.css';
 
 export default function Nav() {
 
     return (
         <nav className="col-5 align-self-center" style={{ backgroundColor: '#EEE' }} >
             <ul className="d-flex flex-column flex-lg-row justify-content-between list-unstyled">
-                <NavLink to={"/"}> Home </NavLink>
-                <NavLink to={"/about"}> About </NavLink>
-                <NavLink to={"/search"}> Search </NavLink>
-                <NavLink to={"/contact"}> Contact Me </NavLink>
-                <NavLink to={"/email-list"}> Email List </NavLink>
+                <NavLink className="nav-item" to={"/"}> Home </NavLink>
+                <NavLink className="nav-item" to={"/about"}> About </NavLink>
+                <NavLink className="nav-item" to={"/search"}> Search </NavLink>
+                <NavLink className="nav-item" to={"/contact"}> Contact Me </NavLink>
+                <NavLink className="nav-item" to={"/email-list"}> Email List </NavLink>
             </ul>
         </nav>
     );
--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import './Post.css';
 
 export default function Post({postData}) {
 
     return (
         <div className="m-4">
-            <Link to={`/posts/${postData.id}`} style={{ textDecoration: 'none' }}>
-                <h2>{postData.title}</h2>
-                <p>{postData.body}</p>
+            <Link className="link-style" to={`/posts/${postData.id}`}>
+                <h2 className="post-title">{postData.title}</h2>
+                <p className="post-body">{postData.body}</p>
                 <img src="https://picsum.photos/200/300" alt="random placeholder" />
             </Link>
         </div>
