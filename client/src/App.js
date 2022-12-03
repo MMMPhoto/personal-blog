30fd1b913cad03a79efe23afe49aee2379e84c1e
Max McDonough <max.mcdonough@gmail.com>
2022-12-03 17:51:12 -0500

    Added Admin page, load home if not logged in

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from './components/Header';
 // import Nav from './components/Nav';
@@ -10,28 +10,43 @@ import EmailList from './pages/EmailList';
 import SinglePost from './pages/SinglePost';
 import Signup from './pages/Signup';
 import Login from './pages/Login';
+import Admin from './pages/Admin';
 import Footer from './components/Footer';
+import Auth from './utils/auth';
 import './App.css';
 
 export default function App() {
-  return (
-    <Router>
-      <div className="d-flex flex-column">
-        <div style={{ backgroundColor: '#EEE' }} >
-          <Header />
-        </div>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/search" element={<Search />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/email-list" element={<EmailList />} />
-          <Route path="/posts/:postId" element={<SinglePost />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
-        <Footer />
-      </div>
-    </Router>
-  );
+	// Set logged in state
+	const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+	// Check login status on load
+	useEffect(() => {
+		const token = Auth.loggedIn() ? Auth.getToken() : null;
+		token ? setIsLoggedIn(true) : setIsLoggedIn(false);
+	}, []);
+
+  	return (
+		<Router>
+			<div className="d-flex flex-column">
+				<div style={{ backgroundColor: '#EEE' }} >
+				<Header />
+				</div>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/about" element={<About />} />
+					<Route path="/search" element={<Search />} />
+					<Route path="/contact" element={<Contact />} />
+					<Route path="/email-list" element={<EmailList />} />
+					<Route path="/posts/:postId" element={<SinglePost />} />
+					<Route path="/signup" element={<Signup />} />
+					<Route path="/login" element={<Login />} />
+					<Route 
+						path="/admin"
+						element={ (isLoggedIn) ? <Admin /> : <Home /> }
+					/>
+				</Routes>
+				<Footer />
+			</div>
+		</Router>
+  	);
 };
---
+++ client/src/pages/Admin.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Admin() {
+
+    return (
+        <div>
+            <h3>Admin Panel</h3>
+        </div>
+    );
+};
