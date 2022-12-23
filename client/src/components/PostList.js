32f0ddc1a07a355f7160cf648356d60e32aa675b
Max McDonough <max.mcdonough@gmail.com>
2022-12-23 14:26:11 -0500

    Added Filter toolbar to post page (non-functional)

--- client/src/components/PostList.js
+++ client/src/components/PostList.js
@@ -6,6 +6,12 @@ import auth from "../utils/auth";
 export default function PostList() {
 
     const [allPosts, setAllPosts] = useState([{}]);
+    const [postFilter, setPostFilter] =useState({
+        sort: "desc by date",
+        published: "all",
+        public: "all",
+        keyword: "",
+    })
     const adminPage = true;
 
     //  Fetch all posts
@@ -26,6 +32,89 @@ export default function PostList() {
 
     return (
         <div>
+            <form style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-evenely' }}>
+                <div className="d-flex flex-row">
+                    <label htmlFor="body">Keyword</label>
+                    <input
+                        // value={userFormData.body}
+                        name="body"
+                        // onChange={handleInputChange}
+                        type="text"
+                    />
+                </div>            
+                <div 
+                    style={{ display: 'flex', flexDirection: 'row', marginLeft: '2vw' }}
+                    // value={postPublic}
+                    >
+                    <p>Visibility:</p>
+                    <div style={{ marginLeft: '1vw' }}>
+                        <input
+                            name="visibility"
+                            value={'both'}
+                            // checked={postPublic === true}
+                            // onChange={() => setPostPublic(true)}
+                            type="radio"
+                        />
+                        <label style={{ marginLeft: '1vw' }} htmlFor="public">All</label>
+                    </div>
+                    <div style={{ marginLeft: '1vw' }}>
+                        <input
+                            name="visibility"
+                            value={'public'}
+                            // checked={postPublic === true}
+                            // onChange={() => setPostPublic(true)}
+                            type="radio"
+                        />
+                        <label style={{ marginLeft: '1vw' }} htmlFor="public">Public</label>
+                    </div>
+                    <div style={{ marginLeft: '1vw' }}>
+                        <input
+                            name="visibility"
+                            value={'private'}
+                            // checked={postPublic === false}
+                            // onChange={() => setPostPublic(false)}
+                            type="radio"
+                        />
+                        <label style={{ marginLeft: '1vw' }} htmlFor="private">Private</label>
+                    </div>
+                </div>
+                <div 
+                    style={{ display: 'flex', flexDirection: 'row', marginLeft: '2vw' }}
+                    // value={postPublic}
+                    >
+                    <p>Published:</p>
+                    <div style={{ marginLeft: '1vw' }}>
+                        <input
+                            name="published"
+                            value={'both'}
+                            // checked={postPublic === true}
+                            // onChange={() => setPostPublic(true)}
+                            type="radio"
+                        />
+                        <label style={{ marginLeft: '1vw' }} htmlFor="public">All</label>
+                    </div>
+                    <div style={{ marginLeft: '1vw' }}>
+                        <input
+                            name="published"
+                            value={'published'}
+                            // checked={postPublic === true}
+                            // onChange={() => setPostPublic(true)}
+                            type="radio"
+                        />
+                        <label style={{ marginLeft: '1vw' }} htmlFor="public">Published</label>
+                    </div>
+                    <div style={{ marginLeft: '1vw' }}>
+                        <input
+                            name="published"
+                            value={'draft'}
+                            // checked={postPublic === false}
+                            // onChange={() => setPostPublic(false)}
+                            type="radio"
+                        />
+                        <label style={{ marginLeft: '1vw' }} htmlFor="private">Draft</label>
+                    </div>
+                </div>
+            </form>
             { allPosts && allPosts.map((post, index) => (
                 <Post key={index} postData={post} adminPage={adminPage} />
             ))}
